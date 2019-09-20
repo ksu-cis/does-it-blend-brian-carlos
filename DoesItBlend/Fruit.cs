863d1750481e2bf468baeb184b3c0eb492012e75
Brian Carlos <briancarlos@ksu.edu>
2019-09-20 14:29:14 -0500

    done.

--- DoesItBlend/Fruit.cs
+++ DoesItBlend/Fruit.cs
@@ -7,7 +7,9 @@ namespace DoesItBlend
     /// <summary>
     /// A base class representing fruit
     /// </summary>
-    public abstract class Fruit
+    public abstract class Fruit : IBlendable
     {
+        // Virtual means derived classes have the OPTION to override
+        public abstract string Blend();
     }
 }
